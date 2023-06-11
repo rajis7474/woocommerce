34d4d6ff4fc09eccd7f9a26306fb61cad58d8421
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-06-11 12:07:11 +0800

    Stepify create-coupon spec

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/create-coupon.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/create-coupon.spec.js
@@ -13,41 +13,59 @@ test.describe( 'Add New Coupon Page', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api.get( 'coupons' ).then( ( response ) => {
-			for ( let i = 0; i < response.data.length; i++ ) {
-				if ( response.data[ i ].code === couponCode ) {
-					api.delete( `coupons/${ response.data[ i ].id }`, {
-						force: true,
-					} );
+
+		await test.step( `Delete test coupons.`, async () => {
+			await api.get( 'coupons' ).then( ( response ) => {
+				for ( let i = 0; i < response.data.length; i++ ) {
+					if ( response.data[ i ].code === couponCode ) {
+						api.delete( `coupons/${ response.data[ i ].id }`, {
+							force: true,
+						} );
+					}
 				}
-			}
+			} );
 		} );
 	} );
 
 	test( 'can create new coupon', async ( { page } ) => {
-		await page.goto( 'wp-admin/post-new.php?post_type=shop_coupon' );
+		await test.step( `Go to Coupons page.`, async () => {
+			await page.goto( 'wp-admin/post-new.php?post_type=shop_coupon' );
+		} );
 
-		await page.locator( '#title' ).fill( couponCode );
+		await test.step( `Fill coupon code with value ${ couponCode }`, async () => {
+			await page.locator( '#title' ).fill( couponCode );
 
-		// Blur then wait for the auto-save to finish
-		await page.locator( '#title' ).blur();
-		await expect(
-			page.getByRole( 'link', { name: 'Move to Trash' } )
-		).toBeVisible();
+			await test.step( `Blur then wait for the auto-save to finish.`, async () => {
+				await page.locator( '#title' ).blur();
 
-		await page
-			.locator( '#woocommerce-coupon-description' )
-			.fill( 'test coupon' );
+				// 'Move to trash' link indicates auto-save success.
+				await expect(
+					page.getByRole( 'link', { name: 'Move to Trash' } )
+				).toBeVisible();
+			} );
+		} );
 
-		await page.locator( '#coupon_amount' ).fill( '100' );
+		await test.step( `Fill coupon description.`, async () => {
+			await page
+				.locator( '#woocommerce-coupon-description' )
+				.fill( 'test coupon' );
+		} );
 
-		await page.locator( '#publish:not(.disabled)' ).click();
-		await page.waitForLoadState( 'networkidle' );
+		await test.step( `Fill coupon amount.`, async () => {
+			await page.locator( '#coupon_amount' ).fill( '100' );
+		} );
 
-		await expect(
-			page
-				.locator( 'div.notice-success > p' )
-				.filter( { hasText: 'Coupon updated.' } )
-		).toBeVisible();
+		await test.step( `Click 'Publish'.`, async () => {
+			await page.locator( '#publish:not(.disabled)' ).click();
+			await page.waitForLoadState( 'networkidle' );
+		} );
+
+		await test.step( `Expect 'Coupon updated' notice.`, async () => {
+			await expect(
+				page
+					.locator( 'div.notice-success > p' )
+					.filter( { hasText: 'Coupon updated.' } )
+			).toBeVisible();
+		} );
 	} );
 } );
