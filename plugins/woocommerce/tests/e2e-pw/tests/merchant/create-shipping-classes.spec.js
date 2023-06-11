feda5cd59c050e24035e38d58fa9a7bccf681eaf
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-06-11 14:33:18 +0800

    Stepify create-shipping-classes spec

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/create-shipping-classes.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/create-shipping-classes.spec.js
@@ -1,72 +1,111 @@
 const { test, expect } = require( '@playwright/test' );
 
+const shippingClassSlug = {
+	name: 'Small Items',
+	slug: 'small-items',
+	description: "Small items that don't cost much to ship.",
+};
+const shippingClassNoSlug = {
+	name: 'Poster Pack',
+	slug: '',
+	description: '',
+};
+const shippingClasses = [ shippingClassSlug, shippingClassNoSlug ];
+
 test.describe( 'Merchant can add shipping classes', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 
 	test.afterEach( async ( { page } ) => {
-		// no api endpoints for shipping classes, so use the UI to cleanup
-		await page.goto(
-			'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
-		);
+		// No api endpoints for shipping classes, so use the UI to cleanup
+		await test.step( `Go to WooCommerce > Settings > Shipping > Shipping classes`, async () => {
+			await page.goto(
+				'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
+			);
+		} );
 
-		await page.dispatchEvent(
-			'.wc-shipping-class-delete >> nth=0',
-			'click'
-		);
-		await page.dispatchEvent(
-			'.wc-shipping-class-delete >> nth=0',
-			'click'
-		);
-		await page.dispatchEvent( 'text=Save shipping classes', 'click' );
+		for ( const sClass of shippingClasses ) {
+			await test.step( `Delete shipping class "${ sClass.name }".`, async () => {
+				const row = page.getByRole( 'row', {
+					name: sClass.name,
+				} );
+				await row.hover();
+				await row.getByRole( 'link', { name: 'Remove' } ).click();
+			} );
+		}
+
+		await test.step( `Click 'Save shipping classes'.`, async () => {
+			await page.locator( 'text=Save shipping classes' ).click();
+		} );
 	} );
 
 	test( 'can add shipping classes', async ( { page } ) => {
-		await page.goto(
-			'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
-		);
+		await test.step( `Go to WooCommerce > Settings > Shipping > Shipping classes`, async () => {
+			await page.goto(
+				'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
+			);
+		} );
 
-		const shippingClassSlug = {
-			name: 'Small Items',
-			slug: 'small-items',
-			description: "Small items that don't cost much to ship.",
-		};
-		const shippingClassNoSlug = {
-			name: 'Poster Pack',
-			slug: '',
-			description: '',
-		};
-		const shippingClasses = [ shippingClassSlug, shippingClassNoSlug ];
+		await test.step( `Add shipping classes.`, async () => {
+			for ( const { name, slug, description } of shippingClasses ) {
+				await test.step( `Click 'Add shipping class' button.`, async () => {
+					await page.locator( 'text=Add shipping class' ).click();
+				} );
 
-		// Add shipping classes
-		for ( const { name, slug, description } of shippingClasses ) {
-			await page.locator( 'text=Add shipping class' ).click();
-			await page
-				.locator( '.editing:last-child [data-attribute="name"]' )
-				.fill( name );
-			await page
-				.locator( '.editing:last-child [data-attribute="slug"]' )
-				.fill( slug );
-			await page
-				.locator( '.editing:last-child [data-attribute="description"]' )
-				.fill( description );
-		}
-		await page.locator( 'text=Save shipping classes' ).click();
+				await test.step( `Fill shipping class name with value "${ name }"`, async () => {
+					await page
+						.locator(
+							'.editing:last-child [data-attribute="name"]'
+						)
+						.fill( name );
+				} );
 
-		// Set the expected auto-generated slug
-		shippingClassNoSlug.slug = 'poster-pack';
+				await test.step( `Fill slug with value "${ slug }"`, async () => {
+					await page
+						.locator(
+							'.editing:last-child [data-attribute="slug"]'
+						)
+						.fill( slug );
+				} );
 
-		// Verify that the specified shipping classes were saved
-		for ( const { name, slug, description } of shippingClasses ) {
-			await expect(
-				page.locator( `text=${ name } Edit | Remove` )
-			).toBeVisible();
-			await expect( page.locator( `text=${ slug }` ) ).toBeVisible();
-			// account for blank description
-			if ( description !== '' ) {
-				await expect(
-					page.locator( `text=${ description }` )
-				).toBeVisible();
+				await test.step( `Fill description with value "${ description }"`, async () => {
+					await page
+						.locator(
+							'.editing:last-child [data-attribute="description"]'
+						)
+						.fill( description );
+				} );
 			}
-		}
+
+			await test.step( `Click 'Save shipping classes' button.`, async () => {
+				await page.locator( 'text=Save shipping classes' ).click();
+			} );
+		} );
+
+		await test.step( `Verify that the specified shipping classes were saved.`, async () => {
+			// Set the expected auto-generated slug
+			shippingClassNoSlug.slug = 'poster-pack';
+
+			for ( const { name, slug, description } of shippingClasses ) {
+				await test.step( `Verify saved name to be "${ name }"`, async () => {
+					await expect(
+						page.locator( `text=${ name } Edit | Remove` )
+					).toBeVisible();
+				} );
+
+				await test.step( `Verify saved slug to be "${ slug }"`, async () => {
+					await expect(
+						page.locator( `text=${ slug }` )
+					).toBeVisible();
+				} );
+
+				if ( description !== '' ) {
+					await test.step( `Verify saved description to be "${ description }"`, async () => {
+						await expect(
+							page.locator( `text=${ description }` )
+						).toBeVisible();
+					} );
+				}
+			}
+		} );
 	} );
 } );
