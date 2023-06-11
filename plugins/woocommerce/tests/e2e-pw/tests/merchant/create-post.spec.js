8b4949c7fef587fa5eaf0bae3f448b736e961b3f
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-06-11 13:50:53 +0800

    Stepify create-post test

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/create-post.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/create-post.spec.js
@@ -17,72 +17,91 @@ test.describe( 'Can create a new post', () => {
 			},
 		} );
 
-		let response = await wpApi.get( `posts` );
-		const allPosts = await response.json();
+		const allPosts = await test.step( `Get all posts.`, async () => {
+			let response = await wpApi.get( `posts` );
 
-		await allPosts.forEach( async ( post ) => {
-			if ( post.title.rendered === postTitle ) {
-				response = await wpApi.delete( `posts/${ post.id }`, {
-					data: {
-						force: true,
-					},
-				} );
-//				expect( response.ok() ).toBeTruthy();
-			}
+			return await response.json();
+		} );
+
+		await test.step( `Clean up posts with title "${ postTitle }".`, async () => {
+			await allPosts.forEach( async ( post ) => {
+				if ( post.title.rendered === postTitle ) {
+					response = await wpApi.delete( `posts/${ post.id }`, {
+						data: {
+							force: true,
+						},
+					} );
+				}
+			} );
 		} );
 	} );
 
 	test( 'can create new post', async ( { page } ) => {
-		await page.goto( 'wp-admin/post-new.php' );
+		await test.step( `Go to Posts > Add New.`, async () => {
+			await page.goto( 'wp-admin/post-new.php' );
+		} );
 
-		const welcomeModalVisible = await test.step(
-			'Check if the Welcome modal appeared',
-			async () => {
-				return await page
-					.getByRole( 'heading', {
-						name: 'Welcome to the block editor',
-					} )
-					.isVisible();
+		await test.step( `Close welcome modal if visible.`, async () => {
+			const welcomeModalVisible =
+				await test.step( 'Check if the Welcome modal appeared', async () => {
+					return await page
+						.getByRole( 'heading', {
+							name: 'Welcome to the block editor',
+						} )
+						.isVisible();
+				} );
+
+			if ( welcomeModalVisible ) {
+				await test.step( 'Welcome modal appeared. Close it.', async () => {
+					await page
+						.getByRole( 'document' )
+						.getByRole( 'button', { name: 'Close' } )
+						.click();
+				} );
+			} else {
+				await test.step( 'Welcome modal did not appear.', async () => {
+					// do nothing.
+				} );
 			}
-		);
+		} );
 
-		if ( welcomeModalVisible ) {
-			await test.step( 'Welcome modal appeared. Close it.', async () => {
-				await page
-					.getByRole( 'document' )
-					.getByRole( 'button', { name: 'Close' } )
-					.click();
-			} );
-		} else {
-			await test.step( 'Welcome modal did not appear.', async () => {
-				// do nothing.
-			} );
-		}
+		await test.step( `Add title.`, async () => {
+			await page
+				.getByRole( 'textbox', { name: 'Add Title' } )
+				.fill( postTitle );
+		} );
 
-		await page
-			.getByRole( 'textbox', { name: 'Add Title' } )
-			.fill( postTitle );
+		await test.step( `Add an empty block.`, async () => {
+			await page
+				.getByRole( 'button', { name: 'Add default block' } )
+				.click();
+		} );
 
-		await page.getByRole( 'button', { name: 'Add default block' } ).click();
+		await test.step( `Fill block contents.`, async () => {
+			await page
+				.getByRole( 'document', {
+					name: 'Empty block; start writing or type forward slash to choose a block',
+				} )
+				.fill( 'Test Post' );
+		} );
 
-		await page
-			.getByRole( 'document', {
-				name:
-					'Empty block; start writing or type forward slash to choose a block',
-			} )
-			.fill( 'Test Post' );
+		await test.step( `Click 'Publish' from the header.`, async () => {
+			await page
+				.getByRole( 'button', { name: 'Publish', exact: true } )
+				.click();
+		} );
 
-		await page
-			.getByRole( 'button', { name: 'Publish', exact: true } )
-			.click();
+		await test.step( `Click 'Publish' from the sliding panel.`, async () => {
+			await page
+				.getByRole( 'region', { name: 'Editor publish' } )
+				.getByRole( 'button', { name: 'Publish', exact: true } )
+				.click();
+		} );
 
-		await page
-			.getByRole( 'region', { name: 'Editor publish' } )
-			.getByRole( 'button', { name: 'Publish', exact: true } )
-			.click();
-
-		await expect(
-			page.getByText( `${ postTitle } is now live.` )
-		).toBeVisible();
+		await test.step( `Expect the message "${ postTitle } is now live." to appear.`, async () => {
+			await expect(
+				page.getByText( `${ postTitle } is now live.` )
+			).toBeVisible();
+		} );
 	} );
 } );
