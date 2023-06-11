c3c733e431b24ca313c403b23749534ba3a7e4e4
Rodel <rodel.calasagsag@automattic.com>
2023-06-11 20:40:27 +0800

    Add steps to `can create simple virtual product` test

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/create-simple-product.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/create-simple-product.spec.js
@@ -10,76 +10,102 @@ test.describe.serial( 'Add New Simple Product Page', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 
 	test.beforeAll( async ( { baseURL } ) => {
-		// need to add a shipping zone
 		const api = new wcApi( {
 			url: baseURL,
 			consumerKey: process.env.CONSUMER_KEY,
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		// and the flat rate shipping method to that zone
-		await api
-			.post( 'shipping/zones', {
-				name: 'Somewhere',
-			} )
-			.then( ( response ) => {
-				shippingZoneId = response.data.id;
-				api.put( `shipping/zones/${ shippingZoneId }/locations`, [
-					{ code: 'CN' },
-				] );
-				api.post( `shipping/zones/${ shippingZoneId }/methods`, {
-					method_id: 'flat_rate',
+
+		await test.step( `Add shipping zone with flat rate shipping method.`, async () => {
+			await api
+				.post( 'shipping/zones', {
+					name: 'Somewhere',
+				} )
+				.then( async ( response ) => {
+					shippingZoneId = response.data.id;
+					await api.put(
+						`shipping/zones/${ shippingZoneId }/locations`,
+						[ { code: 'CN' } ]
+					);
+					await api.post(
+						`shipping/zones/${ shippingZoneId }/methods`,
+						{
+							method_id: 'flat_rate',
+						}
+					);
 				} );
-			} );
+		} );
 	} );
 
 	test.afterAll( async ( { baseURL } ) => {
-		// cleans up all products after run
 		const api = new wcApi( {
 			url: baseURL,
 			consumerKey: process.env.CONSUMER_KEY,
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api.delete( `products/${ virtualProductId }`, { force: true } );
-		await api.delete( `products/${ nonVirtualProductId }`, {
-			force: true,
+
+		await test.step( `Clean up all test products.`, async () => {
+			await api.delete( `products/${ virtualProductId }`, {
+				force: true,
+			} );
+			await api.delete( `products/${ nonVirtualProductId }`, {
+				force: true,
+			} );
 		} );
-		// delete the shipping zone
-		await api.delete( `shipping/zones/${ shippingZoneId }`, {
-			force: true,
+
+		await test.step( `Delete shipping zone.`, async () => {
+			await api.delete( `shipping/zones/${ shippingZoneId }`, {
+				force: true,
+			} );
 		} );
 	} );
 
 	test( 'can create simple virtual product', async ( { page } ) => {
-		await page.goto( 'wp-admin/post-new.php?post_type=product', {
-			waitUntil: 'networkidle',
+		await test.step( `Go to "Add new product" page.`, async () => {
+			await page.goto( 'wp-admin/post-new.php?post_type=product', {
+				waitUntil: 'networkidle',
+			} );
 		} );
-		await page.locator( '#title' ).fill( virtualProductName );
-		await page.locator( '#_regular_price' ).fill( productPrice );
-		await page.locator( '#_virtual' ).click();
-		await page.locator( '#publish' ).click();
-		await page.waitForLoadState( 'networkidle' );
 
-		// When running in parallel, clicking the publish button sometimes saves products as a draft
-		if (
-			(
-				await page.locator( '#post-status-display' ).innerText()
-			 ).includes( 'Draft' )
-		) {
+		await test.step( `Fill product name "${ virtualProductName }".`, async () => {
+			const textbox_productName = page.getByRole( 'textbox', {
+				name: 'Product name',
+			} );
+
+			await textbox_productName.fill( virtualProductName );
+			await textbox_productName.blur();
+			await expect(
+				page.getByRole( 'strong', { name: 'Permalink' } )
+			).toBeVisible();
+		} );
+
+		await test.step( `Fill regular price "${ productPrice }".`, async () => {
+			await page.locator( '#_regular_price' ).fill( productPrice );
+		} );
+
+		await test.step( `Check 'Virtual' checkbox.`, async () => {
+			await page.locator( '#_virtual' ).check();
+		} );
+
+		await test.step( `Click 'Publish' button.`, async () => {
 			await page.locator( '#publish' ).click();
 			await page.waitForLoadState( 'networkidle' );
-		}
+		} );
 
-		await expect(
-			page
-				.locator( 'div.notice-success > p' )
-				.filter( { hasText: 'Product published.' } )
-		).toBeVisible();
+		await test.step( `Expect 'Product published' to be displayed.`, async () => {
+			await expect(
+				page
+					.locator( 'div.notice-success > p' )
+					.filter( { hasText: 'Product published.' } )
+			).toBeVisible();
+		} );
 
-		// Save product ID
-		virtualProductId = page.url().match( /(?<=post=)\d+/ );
-		expect( virtualProductId ).toBeDefined();
+		await test.step( `Save product ID.`, async () => {
+			virtualProductId = page.url().match( /(?<=post=)\d+/ );
+			expect( virtualProductId ).toBeDefined();
+		} );
 	} );
 
 	test( 'can have a shopper add the simple virtual product to the cart', async ( {
