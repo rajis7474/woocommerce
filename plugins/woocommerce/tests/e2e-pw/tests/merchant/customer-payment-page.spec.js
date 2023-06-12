8cf7d9e116d83d986b53188fa08c688d1f8af32c
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-06-12 18:17:26 +0800

    Add step blocks to customer-payment-page spec

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/customer-payment-page.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/customer-payment-page.spec.js
@@ -5,19 +5,18 @@ let productId, orderId;
 const productName = 'Simple Product Name';
 const productPrice = '15.99';
 
-test.describe(
-	'WooCommerce Merchant Flow: Orders > Customer Payment Page',
-	() => {
-		test.use( { storageState: process.env.ADMINSTATE } );
+test.describe( 'WooCommerce Merchant Flow: Orders > Customer Payment Page', () => {
+	test.use( { storageState: process.env.ADMINSTATE } );
 
-		test.beforeAll( async ( { baseURL } ) => {
-			const api = new wcApi( {
-				url: baseURL,
-				consumerKey: process.env.CONSUMER_KEY,
-				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
-			} );
-			// create a simple product
+	test.beforeAll( async ( { baseURL } ) => {
+		const api = new wcApi( {
+			url: baseURL,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			version: 'wc/v3',
+		} );
+
+		await test.step( `Create a simple product`, async () => {
 			await api
 				.post( 'products', {
 					name: productName,
@@ -27,7 +26,9 @@ test.describe(
 				.then( ( response ) => {
 					productId = response.data.id;
 				} );
-			// create an order
+		} );
+
+		await test.step( `Create an order`, async () => {
 			await api
 				.post( 'orders', {
 					line_items: [
@@ -40,84 +41,92 @@ test.describe(
 				.then( ( response ) => {
 					orderId = response.data.id;
 				} );
-			// enable bank transfer as a payment option
+		} );
+
+		await test.step( `Enable bank transfer as a payment option`, async () => {
 			await api.put( 'payment_gateways/bacs', {
 				enabled: 'true',
 			} );
 		} );
+	} );
 
-		test.afterAll( async ( { baseURL } ) => {
-			const api = new wcApi( {
-				url: baseURL,
-				consumerKey: process.env.CONSUMER_KEY,
-				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
-			} );
-			await api.delete( `products/${ productId }`, { force: true } );
-			await api.delete( `orders/${ orderId }`, { force: true } );
-			await api.put( 'payment_gateways/bacs', { enabled: 'false' } );
+	test.afterAll( async ( { baseURL } ) => {
+		const api = new wcApi( {
+			url: baseURL,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			version: 'wc/v3',
 		} );
 
-		test( 'should show the customer payment page link on a pending order', async ( {
-			page,
-		} ) => {
+		await test.step( `Delete test products, orders, and toggle BACS off.`, async () => {
+			await api.delete( `products/${ productId }`, { force: true } );
+			await api.delete( `orders/${ orderId }`, { force: true } );
+		} );
+	} );
+
+	test( 'should show the customer payment page link on a pending order', async ( {
+		page,
+	} ) => {
+		await test.step( `Go to "Edit order" page of order ID ${ orderId }.`, async () => {
 			await page.goto(
 				`wp-admin/post.php?post=${ orderId }&action=edit`
 			);
+		} );
 
-			// verify that the order is pending payment
+		await test.step( `Verify that the order is pending payment`, async () => {
 			await expect(
 				page.locator( '#select2-order_status-container' )
 			).toContainText( 'Pending payment' );
+		} );
 
-			//verify that the customer payment page link is displayed
+		await test.step( `Verify that the customer payment page link is displayed`, async () => {
 			await expect(
 				page.locator( 'label[for=order_status] > a' )
 			).toContainText( 'Customer payment page →' );
 		} );
 
-		test( 'should load the customer payment page', async ( { page } ) => {
-			await page.goto(
-				`wp-admin/post.php?post=${ orderId }&action=edit`
-			);
-
-			// visit the page
+		await test.step( `Click 'Customer payment page'.`, async () => {
 			await page.locator( 'label[for=order_status] > a' ).click();
+		} );
 
-			// verify we landed on the customer payment page
+		await test.step( `Verify we landed on the customer payment page`, async () => {
 			await expect( page.locator( 'h1.entry-title' ) ).toContainText(
 				'Pay for order'
 			);
+		} );
+
+		await test.step( `Expect product name to be correctly shown.`, async () => {
 			await expect( page.locator( 'td.product-name' ) ).toContainText(
 				productName
 			);
+		} );
+
+		await test.step( `Expect product price to be correctly shown.`, async () => {
 			await expect(
 				page.locator( 'span.woocommerce-Price-amount.amount >> nth=0' )
 			).toContainText( productPrice );
 		} );
 
-		test( 'can pay for the order through the customer payment page', async ( {
-			page,
-		} ) => {
-			// key required, so can't go directly to the customer payment page
-			await page.goto(
-				`wp-admin/post.php?post=${ orderId }&action=edit`
-			);
-			await page.locator( 'label[for=order_status] > a' ).click();
-
-			// pay for the order
+		await test.step( `Click 'Pay for order'.`, async () => {
 			await page.locator( 'button#place_order' ).click();
+		} );
 
-			// Verify we landed on the order received page
+		await test.step( `Verify we landed on the order received page`, async () => {
 			await expect( page.locator( 'h1.entry-title' ) ).toContainText(
 				'Order received'
 			);
+		} );
+
+		await test.step( `Expect order ID to be shown.`, async () => {
 			await expect(
 				page.locator( 'li.woocommerce-order-overview__order.order' )
 			).toContainText( orderId.toString() );
+		} );
+
+		await test.step( `Expect product price to be shown.`, async () => {
 			await expect(
 				page.locator( 'span.woocommerce-Price-amount.amount >> nth=0' )
 			).toContainText( productPrice );
 		} );
-	}
-);
+	} );
+} );
