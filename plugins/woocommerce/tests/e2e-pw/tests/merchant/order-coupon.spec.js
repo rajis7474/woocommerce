ac95dfbae65b16b28bccb0841093f9c2acd14785
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-06-12 19:16:27 +0800

    Add step blocks to order-coupon spec

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/order-coupon.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/order-coupon.spec.js
@@ -19,131 +19,168 @@ test.describe( 'WooCommerce Orders > Apply Coupon', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		// create a simple product
-		await api
-			.post( 'products', {
-				name: productName,
-				type: 'simple',
-				regular_price: productPrice,
-			} )
-			.then( ( response ) => {
-				productId = response.data.id;
-			} );
-		// create a $5 off coupon
-		await api
-			.post( 'coupons', {
-				code: couponCode,
-				discount_type: 'fixed_product',
-				amount: couponAmount,
-			} )
-			.then( ( response ) => {
-				couponId = response.data.id;
-			} );
-		// create order
-		await api
-			.post( 'orders', {
-				line_items: [
-					{
-						product_id: productId,
-						quantity: 1,
-					},
-				],
-				coupon_lines: [
-					{
-						code: couponCode,
-					},
-				],
-			} )
-			.then( ( response ) => {
-				orderId = response.data.id;
-			} );
+		await test.step( `Create a simple product`, async () => {
+			await api
+				.post( 'products', {
+					name: productName,
+					type: 'simple',
+					regular_price: productPrice,
+				} )
+				.then( ( response ) => {
+					productId = response.data.id;
+				} );
+		} );
+
+		await test.step( `Create a $5 off coupon`, async () => {
+			await api
+				.post( 'coupons', {
+					code: couponCode,
+					discount_type: 'fixed_product',
+					amount: couponAmount,
+				} )
+				.then( ( response ) => {
+					couponId = response.data.id;
+				} );
+		} );
+
+		await test.step( `Create order`, async () => {
+			await api
+				.post( 'orders', {
+					line_items: [
+						{
+							product_id: productId,
+							quantity: 1,
+						},
+					],
+					coupon_lines: [
+						{
+							code: couponCode,
+						},
+					],
+				} )
+				.then( ( response ) => {
+					orderId = response.data.id;
+				} );
+		} );
 	} );
 
 	test.afterAll( async ( { baseURL } ) => {
-		// cleans up product, coupon and order after run
 		const api = new wcApi( {
 			url: baseURL,
 			consumerKey: process.env.CONSUMER_KEY,
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api.delete( `products/${ productId }`, { force: true } );
-		await api.delete( `coupons/${ couponId }`, { force: true } );
-		await api.delete( `orders/${ orderId }`, { force: true } );
+
+		await test.step( `Clean up product, coupon and order after run`, async () => {
+			await api.delete( `products/${ productId }`, { force: true } );
+			await api.delete( `coupons/${ couponId }`, { force: true } );
+			await api.delete( `orders/${ orderId }`, { force: true } );
+		} );
 	} );
 
 	test( 'can apply a coupon', async ( { page } ) => {
-		await page.goto( 'wp-admin/post-new.php?post_type=shop_order' );
+		await test.step( `Go to "Add new order" page.`, async () => {
+			await page.goto( 'wp-admin/post-new.php?post_type=shop_order' );
+		} );
 
-		// open modal for adding line items
-		await page.locator( 'button.add-line-item' ).click();
-		await page.locator( 'button.add-order-item' ).click();
+		await test.step( `Open modal for adding line items`, async () => {
+			await page.locator( 'button.add-line-item' ).click();
+			await page.locator( 'button.add-order-item' ).click();
+		} );
 
-		// search for product to add
-		await page.locator( 'text=Search for a product…' ).click();
-		await page
-			.locator( '.select2-search--dropdown' )
-			.getByRole( 'combobox' )
-			.type( productName );
-		await page
-			.locator(
-				'li.select2-results__option.select2-results__option--highlighted'
-			)
-			.click();
+		await test.step( `Search for product to add.`, async () => {
+			await page.locator( 'text=Search for a product…' ).click();
+			await page
+				.locator( '.select2-search--dropdown' )
+				.getByRole( 'combobox' )
+				.type( productName );
+			await page
+				.locator(
+					'li.select2-results__option.select2-results__option--highlighted'
+				)
+				.click();
+		} );
 
-		await page.locator( 'button#btn-ok' ).click();
+		await test.step( `Click 'Add'.`, async () => {
+			await page.locator( 'button#btn-ok' ).click();
+		} );
 
-		// apply coupon
-		page.on( 'dialog', ( dialog ) => dialog.accept( couponCode ) );
-		await page.locator( 'button.add-coupon' ).click();
+		await test.step( `Apply coupon.`, async () => {
+			page.on( 'dialog', ( dialog ) => dialog.accept( couponCode ) );
+			await page.locator( 'button.add-coupon' ).click();
+		} );
 
-		await expect(
-			page.locator( '.wc_coupon_list li', { hasText: couponCode } )
-		).toBeVisible();
-		await expect(
-			page.locator( '.wc-order-totals td.label >> nth=1' )
-		).toContainText( 'Coupon(s)' );
-		await expect(
-			page.locator( '.wc-order-totals td.label >> nth=2' )
-		).toContainText( 'Order Total' );
-		await expect(
-			page.locator( '.wc-order-totals td.total >> nth=1' )
-		).toContainText( couponAmount );
-		await expect(
-			page.locator( '.wc-order-totals td.total >> nth=2' )
-		).toContainText( discountedPrice );
+		await test.step( `Expect coupon code to appear on coupon list.`, async () => {
+			await expect(
+				page.locator( '.wc_coupon_list li', { hasText: couponCode } )
+			).toBeVisible();
+		} );
+
+		await test.step( `Expect 'Coupon(s)' line to be displayed.`, async () => {
+			await expect(
+				page.locator( '.wc-order-totals td.label >> nth=1' )
+			).toContainText( 'Coupon(s)' );
+		} );
+
+		await test.step( `Expect 'Order Total' line to be displayed.`, async () => {
+			await expect(
+				page.locator( '.wc-order-totals td.label >> nth=2' )
+			).toContainText( 'Order Total' );
+		} );
+
+		await test.step( `Expect coupon amount to be correct.`, async () => {
+			await expect(
+				page.locator( '.wc-order-totals td.total >> nth=1' )
+			).toContainText( couponAmount );
+		} );
+
+		await test.step( `Expect discounted price to be correct.`, async () => {
+			await expect(
+				page.locator( '.wc-order-totals td.total >> nth=2' )
+			).toContainText( discountedPrice );
+		} );
 	} );
 
 	test( 'can remove a coupon', async ( { page } ) => {
-		await page.goto( `/wp-admin/post.php?post=${ orderId }&action=edit` );
-		// assert that there is a coupon on the order
-		await expect(
-			page.locator( '.wc_coupon_list li', { hasText: couponCode } )
-		).toBeVisible();
-		await expect(
-			page.locator( '.wc-order-totals td.label >> nth=1' )
-		).toContainText( 'Coupon(s)' );
-		await expect(
-			page.locator( '.wc-order-totals td.label >> nth=2' )
-		).toContainText( 'Order Total' );
-		await expect(
-			page.locator( '.wc-order-totals td.total >> nth=1' )
-		).toContainText( couponAmount );
-		await expect(
-			page.locator( '.wc-order-totals td.total >> nth=2' )
-		).toContainText( discountedPrice );
-		// remove the coupon
-		await page.dispatchEvent( 'a.remove-coupon', 'click' ); // have to use dispatchEvent because nothing visible to click on
+		await test.step( `Go to "Edit order" page of order ID ${ orderId }.`, async () => {
+			await page.goto(
+				`/wp-admin/post.php?post=${ orderId }&action=edit`
+			);
+		} );
 
-		// make sure the coupon was removed
-		await expect(
-			page.locator( '.wc_coupon_list li', { hasText: couponCode } )
-		).not.toBeVisible();
-		await expect(
-			page.locator( '.wc-order-totals td.label >> nth=1' )
-		).toContainText( 'Order Total' );
-		await expect(
-			page.locator( '.wc-order-totals td.total >> nth=1' )
-		).toContainText( productPrice );
+		await test.step( `Assert that there is a coupon on the order`, async () => {
+			await expect(
+				page.locator( '.wc_coupon_list li', { hasText: couponCode } )
+			).toBeVisible();
+			await expect(
+				page.locator( '.wc-order-totals td.label >> nth=1' )
+			).toContainText( 'Coupon(s)' );
+			await expect(
+				page.locator( '.wc-order-totals td.label >> nth=2' )
+			).toContainText( 'Order Total' );
+			await expect(
+				page.locator( '.wc-order-totals td.total >> nth=1' )
+			).toContainText( couponAmount );
+			await expect(
+				page.locator( '.wc-order-totals td.total >> nth=2' )
+			).toContainText( discountedPrice );
+		} );
+
+		await test.step( `Remove coupon.`, async () => {
+			await page.locator( 'a.remove-coupon' ).dispatchEvent( 'click' );
+		} );
+
+		await test.step( `Make sure the coupon was removed`, async () => {
+			await expect(
+				page.locator( '.wc_coupon_list li', { hasText: couponCode } )
+			).not.toBeVisible();
+			await expect(
+				page.locator( '.wc-order-totals td.label >> nth=1' )
+			).toContainText( 'Order Total' );
+			await expect(
+				page.locator( '.wc-order-totals td.total >> nth=1' )
+			).toContainText( productPrice );
+		} );
 	} );
 } );
