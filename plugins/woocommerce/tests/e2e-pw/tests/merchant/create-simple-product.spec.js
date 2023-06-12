054c6d43d35e72deb1f9a0f35464b8763b88b5a8
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-06-12 17:29:15 +0800

    Add step blocks in create-simple-product spec

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/create-simple-product.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/create-simple-product.spec.js
@@ -47,11 +47,8 @@ test.describe.serial( 'Add New Simple Product Page', () => {
 		} );
 
 		await test.step( `Clean up all test products.`, async () => {
-			await api.delete( `products/${ virtualProductId }`, {
-				force: true,
-			} );
-			await api.delete( `products/${ nonVirtualProductId }`, {
-				force: true,
+			await api.post( `products/batch`, {
+				delete: [ virtualProductId, nonVirtualProductId ],
 			} );
 		} );
 
@@ -64,21 +61,19 @@ test.describe.serial( 'Add New Simple Product Page', () => {
 
 	test( 'can create simple virtual product', async ( { page } ) => {
 		await test.step( `Go to "Add new product" page.`, async () => {
-			await page.goto( 'wp-admin/post-new.php?post_type=product', {
-				waitUntil: 'networkidle',
-			} );
+			await page.goto( '/wp-admin/post-new.php?post_type=product' );
 		} );
 
 		await test.step( `Fill product name "${ virtualProductName }".`, async () => {
-			const textbox_productName = page.getByRole( 'textbox', {
-				name: 'Product name',
-			} );
+			await page
+				.getByRole( 'textbox', {
+					name: 'Product name',
+				} )
+				.fill( virtualProductName );
+		} );
 
-			await textbox_productName.fill( virtualProductName );
-			await textbox_productName.blur();
-			await expect(
-				page.getByRole( 'strong', { name: 'Permalink' } )
-			).toBeVisible();
+		await test.step( `Save draft to prevent auto-save from interfering with succeeding steps.`, async () => {
+			await page.getByRole( 'button', { name: 'Save Draft' } ).click();
 		} );
 
 		await test.step( `Fill regular price "${ productPrice }".`, async () => {
@@ -91,7 +86,6 @@ test.describe.serial( 'Add New Simple Product Page', () => {
 
 		await test.step( `Click 'Publish' button.`, async () => {
 			await page.locator( '#publish' ).click();
-			await page.waitForLoadState( 'networkidle' );
 		} );
 
 		await test.step( `Expect 'Product published' to be displayed.`, async () => {
@@ -103,7 +97,10 @@ test.describe.serial( 'Add New Simple Product Page', () => {
 		} );
 
 		await test.step( `Save product ID.`, async () => {
-			virtualProductId = page.url().match( /(?<=post=)\d+/ );
+			virtualProductId = page
+				.url()
+				.match( /(?<=post=)\d+/ )
+				.at( 0 );
 			expect( virtualProductId ).toBeDefined();
 		} );
 	} );
@@ -111,91 +108,154 @@ test.describe.serial( 'Add New Simple Product Page', () => {
 	test( 'can have a shopper add the simple virtual product to the cart', async ( {
 		page,
 	} ) => {
-		await page.goto( `/?post_type=product&p=${ virtualProductId }`, {
-			waitUntil: 'networkidle',
+		await test.step( `Clear cookies to assure cart items from other tests do not appear on this test.`, async () => {
+			await page.context().clearCookies();
 		} );
-		await expect( page.locator( '.product_title' ) ).toHaveText(
-			virtualProductName
-		);
-		await expect(
-			page.locator( '.summary .woocommerce-Price-amount' )
-		).toContainText( productPrice );
-		await page.getByRole( 'button', { name: 'Add to cart' } ).click();
-		await page.getByRole( 'link', { name: 'View cart' } ).click();
-		await expect( page.locator( 'td[data-title=Product]' ) ).toContainText(
-			virtualProductName
-		);
-		await expect(
-			page.locator( 'a.shipping-calculator-button' )
-		).not.toBeVisible();
-		await page
-			.locator( `a.remove[data-product_id='${ virtualProductId }']` )
-			.click();
-		await page.waitForLoadState( 'networkidle' );
-		await expect(
-			page.locator( `a.remove[data-product_id='${ virtualProductId }']` )
-		).not.toBeVisible();
+
+		await test.step( `Go to product page of "${ virtualProductName }".`, async () => {
+			await page.goto( `/?post_type=product&p=${ virtualProductId }`, {
+				waitUntil: 'networkidle',
+			} );
+
+			await expect( page.locator( '.product_title' ) ).toHaveText(
+				virtualProductName
+			);
+
+			await expect(
+				page.locator( '.summary .woocommerce-Price-amount' )
+			).toContainText( productPrice );
+		} );
+
+		await test.step( `Click 'Add to cart' button.`, async () => {
+			await page.getByRole( 'button', { name: 'Add to cart' } ).click();
+		} );
+
+		await test.step( `Click 'View cart'.`, async () => {
+			await page.getByRole( 'link', { name: 'View cart' } ).click();
+		} );
+
+		await test.step( `Expect "${ virtualProductName }" to be added to cart.`, async () => {
+			await expect(
+				page.locator( 'td[data-title=Product]' )
+			).toContainText( virtualProductName );
+		} );
+
+		await test.step( `Expect the shipping calculator button to not be visibile.`, async () => {
+			await expect(
+				page.locator( 'a.shipping-calculator-button' )
+			).not.toBeVisible();
+		} );
+
+		await test.step( `Click 'Remove' link on "${ virtualProductName }"`, async () => {
+			await page
+				.locator( `a.remove[data-product_id='${ virtualProductId }']` )
+				.click();
+		} );
+
+		await test.step( `Expect "${ virtualProductName }" to be removed from cart.`, async () => {
+			await expect(
+				page.locator(
+					`a.remove[data-product_id='${ virtualProductId }']`
+				)
+			).not.toBeVisible();
+		} );
 	} );
 
 	test( 'can create simple non-virtual product', async ( { page } ) => {
-		await page.goto( 'wp-admin/post-new.php?post_type=product', {
-			waitUntil: 'networkidle',
+		await test.step( `Go to "Add new product" page.`, async () => {
+			await page.goto( 'wp-admin/post-new.php?post_type=product' );
 		} );
-		await page.locator( '#title' ).fill( nonVirtualProductName );
-		await page.locator( '#_regular_price' ).fill( productPrice );
-		await expect( page.locator( '#publish:not(.disabled)' ) ).toBeVisible();
-		await page.locator( '#publish' ).click();
-		await page.waitForLoadState( 'networkidle' );
 
-		// When running in parallel, clicking the publish button sometimes saves products as a draft
-		if (
-			(
-				await page.locator( '#post-status-display' ).innerText()
-			 ).includes( 'Draft' )
-		) {
+		await test.step( `Type "${ nonVirtualProductName }" into the "Product name" textbox.`, async () => {
+			await page.locator( '#title' ).fill( nonVirtualProductName );
+		} );
+
+		await test.step( `Save draft to prevent auto-save from interfering with succeeding steps.`, async () => {
+			await page.getByRole( 'button', { name: 'Save Draft' } ).click();
+		} );
+
+		await test.step( `Type "${ productPrice }" into the "Regular price" textbox.`, async () => {
+			await page.locator( '#_regular_price' ).fill( productPrice );
+		} );
+
+		await test.step( `Click "Publish" button.`, async () => {
 			await page.locator( '#publish' ).click();
-			await page.waitForLoadState( 'networkidle' );
-		}
+		} );
 
-		await expect(
-			page
-				.locator( 'div.notice-success > p' )
-				.filter( { hasText: 'Product published.' } )
-		).toBeVisible();
+		await test.step( `Expect 'Product published' to be displayed.`, async () => {
+			await expect(
+				page
+					.locator( 'div.notice-success > p' )
+					.filter( { hasText: 'Product published.' } )
+			).toBeVisible();
+		} );
 
-		// Save product ID
-		nonVirtualProductId = page.url().match( /(?<=post=)\d+/ );
-		expect( nonVirtualProductId ).toBeDefined();
+		await test.step( `Save product ID.`, async () => {
+			nonVirtualProductId = page
+				.url()
+				.match( /(?<=post=)\d+/ )
+				.at( 0 );
+			expect( nonVirtualProductId ).toBeDefined();
+		} );
 	} );
 
 	test( 'can have a shopper add the simple non-virtual product to the cart', async ( {
 		page,
 	} ) => {
-		await page.goto( `/?post_type=product&p=${ nonVirtualProductId }`, {
-			waitUntil: 'networkidle',
+		await test.step( `Clear cookies to assure cart items from other tests do not appear on this test.`, async () => {
+			await page.context().clearCookies();
 		} );
-		await expect( page.locator( '.product_title' ) ).toHaveText(
-			nonVirtualProductName
-		);
-		await expect(
-			page.locator( '.summary .woocommerce-Price-amount' )
-		).toContainText( productPrice );
-		await page.getByRole( 'button', { name: 'Add to cart' } ).click();
-		await page.getByRole( 'link', { name: 'View cart' } ).click();
-		await expect( page.locator( 'td[data-title=Product]' ) ).toContainText(
-			nonVirtualProductName
-		);
-		await expect(
-			page.locator( 'a.shipping-calculator-button' )
-		).toBeVisible();
-		await page
-			.locator( `a.remove[data-product_id='${ nonVirtualProductId }']` )
-			.click();
-		await page.waitForLoadState( 'networkidle' );
-		await expect(
-			page.locator(
-				`a.remove[data-product_id='${ nonVirtualProductId }']`
-			)
-		).not.toBeVisible();
+
+		await test.step( `As a shopper, go to the product page of "${ nonVirtualProductName }".`, async () => {
+			await page.goto( `/?post_type=product&p=${ nonVirtualProductId }` );
+		} );
+
+		await test.step( `Expect displayed product name to be correct.`, async () => {
+			await expect( page.locator( '.product_title' ) ).toHaveText(
+				nonVirtualProductName
+			);
+		} );
+
+		await test.step( `Expect displayed price amount to be correct.`, async () => {
+			await expect(
+				page.locator( '.summary .woocommerce-Price-amount' )
+			).toContainText( productPrice );
+		} );
+
+		await test.step( `Click 'Add to cart'.`, async () => {
+			await page.getByRole( 'button', { name: 'Add to cart' } ).click();
+		} );
+
+		await test.step( `Click 'View cart'.`, async () => {
+			await page.getByRole( 'link', { name: 'View cart' } ).click();
+		} );
+
+		await test.step( `Expect product "${ nonVirtualProductName }" to be added to the cart.`, async () => {
+			await expect(
+				page.locator( 'td[data-title=Product]' )
+			).toContainText( nonVirtualProductName );
+		} );
+
+		await test.step( `Expect shipping calculator to not be visible.`, async () => {
+			await expect(
+				page.locator( 'a.shipping-calculator-button' )
+			).toBeVisible();
+		} );
+
+		await test.step( `Click 'Remove'.`, async () => {
+			await page
+				.locator(
+					`a.remove[data-product_id='${ nonVirtualProductId }']`
+				)
+				.click();
+		} );
+
+		await test.step( `Expect "${ nonVirtualProductName }" to be removed from the cart.`, async () => {
+			await expect(
+				page.locator(
+					`a.remove[data-product_id='${ nonVirtualProductId }']`
+				)
+			).not.toBeVisible();
+		} );
 	} );
 } );
