608177d53df0bd1031bd691a60233b343fabcbfa
Rodel <rodel.calasagsag@automattic.com>
2023-06-18 21:41:47 +0800

    Add step blocks to product-search

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/product-search.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/product-search.spec.js
@@ -7,6 +7,16 @@ const productName = `Unique thing that we sell ${ new Date()
 	.toString() }`;
 const productPrice = '9.99';
 
+/**
+ *
+ * @param {import('@playwright/test').Page} page
+ * @param {string} searchString
+ */
+const searchFor = async ( page, searchString ) => {
+	await page.locator( '#post-search-input' ).fill( searchString );
+	await page.locator( '#search-submit' ).click();
+};
+
 test.describe( 'Products > Search and View a product', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 
@@ -17,15 +27,18 @@ test.describe( 'Products > Search and View a product', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api
-			.post( 'products', {
-				name: productName,
-				type: 'simple',
-				regular_price: productPrice,
-			} )
-			.then( ( response ) => {
-				productId = response.data.id;
-			} );
+
+		await test.step( `Create test product.`, async () => {
+			await api
+				.post( 'products', {
+					name: productName,
+					type: 'simple',
+					regular_price: productPrice,
+				} )
+				.then( ( response ) => {
+					productId = response.data.id;
+				} );
+		} );
 	} );
 
 	test.afterAll( async ( { baseURL } ) => {
@@ -35,53 +48,74 @@ test.describe( 'Products > Search and View a product', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api.delete( `products/${ productId }`, {
-			force: true,
+
+		await test.step( `Delete test product.`, async () => {
+			await api.delete( `products/${ productId }`, {
+				force: true,
+			} );
+		} );
+	} );
+
+	test.beforeEach( async ( { page } ) => {
+		await test.step( `Go to Products > All products page.`, async () => {
+			await page.goto( 'wp-admin/edit.php?post_type=product' );
 		} );
 	} );
 
 	test( 'can do a partial search for a product', async ( { page } ) => {
-		// create a partial search string
-		const searchString = productName.substring( 0, productName.length / 2 );
+		await test.step( `Search with substring of product name.`, async () => {
+			const searchString = productName.substring(
+				0,
+				productName.length / 2
+			);
 
-		await page.goto( 'wp-admin/edit.php?post_type=product' );
+			await searchFor( page, searchString );
+		} );
 
-		await page.locator( '#post-search-input' ).fill( searchString );
-		await page.locator( '#search-submit' ).click();
-		await page.waitForLoadState( 'networkidle' );
-
-		await expect( page.locator( '.row-title' ) ).toContainText(
-			productName
-		);
+		await test.step( `Expect search result to show product name.`, async () => {
+			await expect( page.locator( '.row-title' ) ).toContainText(
+				productName
+			);
+		} );
 	} );
 
 	test( "can view a product's details after search", async ( { page } ) => {
 		const productIdInURL = new RegExp( `post=${ productId }` );
 
-		await page.goto( 'wp-admin/edit.php?post_type=product' );
+		await test.step( `Search with product name.`, async () => {
+			await searchFor( page, productName );
+		} );
 
-		await page.locator( '#post-search-input' ).fill( productName );
-		await page.locator( '#search-submit' ).click();
+		await test.step( `Click on the matching result.`, async () => {
+			await page.locator( '.row-title' ).click();
+		} );
 
-		await page.locator( '.row-title' ).click();
+		await test.step( `Expect URL to show product ID.`, async () => {
+			await expect( page ).toHaveURL( productIdInURL );
+		} );
 
-		await expect( page ).toHaveURL( productIdInURL );
-		await expect( page.locator( '#title' ) ).toHaveValue( productName );
-		await expect( page.locator( '#_regular_price' ) ).toHaveValue(
-			productPrice
-		);
+		await test.step( `Expect product name to be correct.`, async () => {
+			await expect( page.locator( '#title' ) ).toHaveValue( productName );
+		} );
+
+		await test.step( `Expect product price to be correct.`, async () => {
+			await expect( page.locator( '#_regular_price' ) ).toHaveValue(
+				productPrice
+			);
+		} );
 	} );
 
 	test( 'returns no results for non-existent product search', async ( {
 		page,
 	} ) => {
-		await page.goto( 'wp-admin/edit.php?post_type=product' );
+		await test.step( `Search with random string yielding no matches.`, async () => {
+			await searchFor( page, 'abcd1234' );
+		} );
 
-		await page.locator( '#post-search-input' ).fill( 'abcd1234' );
-		await page.locator( '#search-submit' ).click();
-
-		await expect( page.locator( '.no-items' ) ).toContainText(
-			'No products found'
-		);
+		await test.step( `Expect message saying there were no products found.`, async () => {
+			await expect( page.locator( '.no-items' ) ).toContainText(
+				'No products found'
+			);
+		} );
 	} );
 } );
