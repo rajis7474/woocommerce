00e1a69d21408fac3612fedc12379315855f1387
Rodel <rodel.calasagsag@automattic.com>
2023-06-18 21:10:20 +0800

    Add step blocks to product-edit spec

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/product-edit.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/product-edit.spec.js
@@ -13,15 +13,18 @@ test.describe( 'Products > Edit Product', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api
-			.post( 'products', {
-				name: 'Product to edit',
-				type: 'simple',
-				regular_price: '12.99',
-			} )
-			.then( ( response ) => {
-				productId = response.data.id;
-			} );
+
+		await test.step( `Create test product.`, async () => {
+			await api
+				.post( 'products', {
+					name: 'Product to edit',
+					type: 'simple',
+					regular_price: '12.99',
+				} )
+				.then( ( response ) => {
+					productId = response.data.id;
+				} );
+		} );
 	} );
 
 	test.afterAll( async ( { baseURL } ) => {
@@ -31,34 +34,61 @@ test.describe( 'Products > Edit Product', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		await api.delete( `products/${ productId }`, {
-			force: true,
+
+		await test.step( `Delete test product.`, async () => {
+			await api.delete( `products/${ productId }`, {
+				force: true,
+			} );
 		} );
 	} );
 
 	test( 'can edit a product and save the changes', async ( { page } ) => {
-		await page.goto( `wp-admin/post.php?post=${ productId }&action=edit` );
+		await test.step( `Open test product.`, async () => {
+			await page.goto(
+				`wp-admin/post.php?post=${ productId }&action=edit`
+			);
+		} );
 
-		// make some edits
-		await page.locator( '#title' ).fill( 'Awesome product' );
-		await page.locator( '#content-html' ).click(); // text mode to work around iframe
-		await page
-			.locator( '.wp-editor-area >> nth=0' )
-			.fill( 'This product is pretty awesome' );
-		await page.locator( '#_regular_price' ).fill( '100.05' );
+		await test.step( `Change product title.`, async () => {
+			await page.locator( '#title' ).fill( 'Awesome product' );
+		} );
 
-		// publish the edits
-		await page.locator( '#publish' ).click();
+		await test.step( `Enter product description text mode to work around iframe.`, async () => {
+			await page.locator( '#content-html' ).click();
+		} );
 
-		// verify the changes saved
-		await expect( page.locator( '#title' ) ).toHaveValue(
-			'Awesome product'
-		);
-		await expect(
-			page.locator( '.wp-editor-area >> nth=0' )
-		).toContainText( 'This product is pretty awesome' );
-		await expect( page.locator( '#_regular_price' ) ).toHaveValue(
-			'100.05'
-		);
+		await test.step( `Change product description`, async () => {
+			await page
+				.locator( '.wp-editor-area >> nth=0' )
+				.fill( 'This product is pretty awesome' );
+		} );
+
+		await test.step( `Change regular price.`, async () => {
+			await page.locator( '#_regular_price' ).fill( '100.05' );
+		} );
+
+		await test.step( `Publish the edits`, async () => {
+			await page.locator( '#publish' ).click();
+		} );
+
+		await test.step( `Verify changes saved.`, async () => {
+			await test.step( `Verify product title.`, async () => {
+				await expect( page.locator( '#title' ) ).toHaveValue(
+					'Awesome product'
+				);
+			} );
+
+			await test.step( `Verify product description.`, async () => {
+				await expect(
+					page.locator( '.wp-editor-area >> nth=0' )
+				).toContainText( 'This product is pretty awesome' );
+			} );
+
+			await test.step( `Verify product price.`, async () => {
+				await expect( page.locator( '#_regular_price' ) ).toHaveValue(
+					'100.05'
+				);
+			} );
+		} );
 	} );
 } );
